25349b54ec450d2bb15010d3ad0ab69ecabbe735
Kostiantyn Fesenko <kofesenko@gmail.com>
2022-08-18 16:58:22 +0300

    Added ecr_lifecycle_policy to keep repository clean

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -8,3 +8,25 @@ resource "aws_ecr_repository" "some_app" {
   }
   force_delete = true
 }
+
+#policy to keep only 30 images in ecr
+resource "aws_ecr_lifecycle_policy" "some_app_policy" {
+  repository = aws_ecr_repository.some_app.name
+
+  policy = jsonencode({
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 30 images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 30
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+})
+}
